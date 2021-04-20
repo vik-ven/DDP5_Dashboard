831795231db32c94fd9abd834a8114449706b7e2
vik-ven <vlv7@cornell.edu>
2021-04-19 22:04:43 -0400

    Added model interactions and cleaned up some perf issues

--- streamlitRunner.py
+++ streamlitRunner.py
@@ -5,26 +5,14 @@ from bigml.api import BigML
 from PIL import Image
 import altair as alt
 
-# Downloads and generates a local version of the logistic regression,
-# if it hasn't been downloaded previously.
-
-logisticregression = LogisticRegression('logisticregression/607da8de7ca0e613bd002400',
+logisticregression = LogisticRegression('logisticregression/607e21b3f7af1513af005db6',
                   api=BigML("vlv7",
                             "5bfb457d1ad360008520230eee0c229084b85f04",
                             domain="bigml.io"))
-
-# To predict probabilities fill the desired input_data
-# in next line. Numeric fields are compulsory if the model was not
-# trained with missing numerics.
-#
-# input_data: dict for the input values
-# (e.g. {"petal length": 1, "sepal length": 3})
-# full: if set to True, the output will be a dictionary that includes the
-# distribution of each class in the objective field, the predicted class and
-# its probability. Please check:
-# https://bigml.readthedocs.io/en/latest/#local-logistic-regression-predictions
-@st.cache
+# To make predictions fill the desired input_data in next line.
+@st.cache(suppress_st_warning=True)
 def setup_fxn():
+    #st.header("SetupFXN Run")
     input_data = {
         "director": "Woody Allen",
         "writer": "Woody Allen",
@@ -52,34 +40,47 @@ def setup_fxn():
     #max value for budget
     maxval = int(main_df["budget"].max())
     maxdur = int(main_df["duration"].max())
-
     return main_df, d_df, a_df, w_df, banner, g_df, maxval, maxdur, p_df, modstring
+
 def query(genre, director, writer, actors, budget, duration, prod_comp, month_inp, oscar, metascore):
-    
+    input_data = {
+        "director": ", ".join(x for x in director),
+        "writer": ", ".join(x for x in writer),
+        "genre": ", ".join(x for x in genre),
+        "duration": duration,
+        "Month": month_inp,
+        "production_company": prod_comp,
+        "actors": ", ".join(x for x in actors),
+        "OscarWinner": "TRUE" if oscar == "Yes" else "FALSE",
+        "budget": budget,
+        "Metascore": metascore
+    }
+    log = logisticregression.predict(input_data, full=True)
+    return "Your latest query to the model yielded: **" + log['prediction'] + "** with a probability of **" + str(log['probability']) + "**", log['prediction'],
 
 main_df, d_df, a_df, w_df, banner, g_df, maxval, maxdur, p_df, modstring = setup_fxn()
 month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
 st.image(banner)
 st.title("What Makes a Successful Movie?")
 st.write("Designing Data Products Team 5 is pleased to introduce an analysis on the elements that allow a film to make back its budget")
-scatter = alt.Chart(main_df[['budget', 'worlwide_gross_income']][:1000]).mark_circle().encode(
+scatter = alt.Chart(main_df[['budget', 'worlwide_gross_income']][:700]).mark_circle().encode(
     x = 'budget', y = 'worlwide_gross_income', color = 'budget', tooltip=['budget', 'worlwide_gross_income']
 )
 st.write("As seen here, there's a **huge variability** in budget and worldwide gross, even among high-budget projects...")
 st.altair_chart(scatter)
-st.write("...which is why we *made a model* to help producers and companies figure out what to expect. Put some options in the sidebar to try it out!")
+st.write("...which is why we *made a model* to help producers and companies figure out what to expect. This model predicts if a film makes back at least **1.5x** what was spent on it for a **50% profit margin**. Put some options in the sidebar to try it out!")
 
 #sidebar entry
 genre = st.sidebar.multiselect("Start with a genre, or multiple for your film!", g_df['genre'])
 director = st.sidebar.multiselect("Next, who will direct it?", d_df['director'])
 writer = st.sidebar.multiselect("Choose your writer(s)", w_df['writer'])
-actors = st.sidebar.multiselect("Who will act in your film?", a_df['actor'])
+actors = st.sidebar.multiselect("Who will act in your film? You can choose any number", a_df['actor'])
 budget = st.sidebar.slider("What kind of budget are you looking at?", min_value=1000, max_value=maxval, step=1000)
 duration = st.sidebar.slider("How long will your movie be in minutes?", min_value=1, max_value=maxdur)
 prod_comp = st.sidebar.selectbox("Which company will produce your film?", p_df['production_company'])
 month_inp = st.sidebar.selectbox("What month will it release in?", month)
 oscar = st.sidebar.radio("Will this film win an Oscar?", ('Yes', 'No'), index=1 )
-metascore = st.sidebar.slider("What Metascore should we assign your film?", min_value=0, max_value=100)
+metascore = st.sidebar.slider("What Metascore should we assign your film?", min_value=0, max_value=100, step=5)
 
 #output stuff for everyone
 st.header("Currently you have selected:")
@@ -112,7 +113,19 @@ else:
     st.write("You have chosen that your film will **not win an Oscar**")
 st.write("You have set that your film will recieve a Metacritic Score of **" + str(metascore) + "**")
 click = st.button("Click to Query Model")
+resstring = ""
+predict = ""
 if click:
-    modstring = query(genre, director, writer, actors, budget, duration, prod_comp, month_inp, oscar, metascore)
+    if not actors or not director or not genre or not writer:
+        resstring = "One of actor, writer, director, or genre is not properly filled in, please do so in order to query the logistic model"
+    else:
+        resstring, predict = query(genre, director, writer, actors, budget, duration, prod_comp, month_inp, oscar, metascore)
 st.header("Results")
-st.write(modstring)
+if resstring:
+    st.write(resstring)
+    if predict == "TRUE":
+        st.write("Congrats on the successful movie! Your film is predicted to make at least 1.5x its budget")
+    if predict == "FALSE":
+        st.write("Back to the cinematic drawing board...")
+else:
+    st.write(modstring)
